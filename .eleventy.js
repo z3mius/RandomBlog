862dc7ab83d94252f23208e4c44767fafbc28f1c
Bogdan Grossu <n0dcpen@gmail.com>
2023-06-29 08:13:46 +0200

    added admin

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('src/styles/');
   eleventyConfig.addPassthroughCopy('src/');
+  eleventyConfig.addPassthroughCopy('src/admin');
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
