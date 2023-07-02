c80ea6656e5dc3352b7d0ee7ba60e10fb037dc44
Bogdan Grossu <n0cdpen@gmail.com>
2023-07-02 10:44:59 +0200

    changed

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,16 @@
 const { DateTime } = require("luxon");
-
+const markdownIt = require('markdown-it');
 
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('src/styles/');
   eleventyConfig.addPassthroughCopy('src/');
   eleventyConfig.addPassthroughCopy('src/admin');
+
+  eleventyConfig.addFilter('markdown', function (content) {
+    const markdown = markdownIt();
+    return markdown.render(content);
+  })
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
