fa5cdfa9176e89934e36befb34f75e2329110357
Bogdan Grossu <n0cdpen@gmail.com>
2023-07-05 11:05:48 +0200

    changed

--- .eleventy.js
+++ .eleventy.js
@@ -1,28 +1,18 @@
-// Add above your Eleventy config
-const markdownIt = require("markdown-it");
-
-// Add within your config module
-const md = new markdownIt({
-  html: true,
-});
-
-
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addFilter("markdown", (content) => {
-    return md.render(content);
-  });
 
   eleventyConfig.addPassthroughCopy('src/styles/');
   eleventyConfig.addPassthroughCopy('src/');
   eleventyConfig.addPassthroughCopy('src/admin');
-
-  eleventyConfig.addFilter('markdown', function (content) {
-    const markdown = markdownIt();
-    return markdown.render(content);
+  const md = require("markdown-it")({
+    html: true
   })
+
+  eleventyConfig.addNunjucksFilter("markdownify", (markdownString) =>
+  md.render(markdownString)
+  );
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
--- src/blog/2023-06-29-how-to-write-in-c.md
+++ src/blog/2023-06-29-how-to-write-in-c.md
@@ -6,11 +6,4 @@ date: 2023-06-29T14:21:49.626Z
 tags:
   - post
 ---
-* ```cpp
-  include #whatever
-  main ()
-  ```
-
-  1. h﻿ello
-  2. g﻿ood morning
-  3. w﻿hat's up
+hello
