8f4936f0b06cb5a4301fb757623bacd72d7e969a
Bogdan Grossu <n0cdpen@gmail.com>
2023-07-10 15:18:42 +0200

    changes

--- .eleventy.js
+++ .eleventy.js
@@ -6,13 +6,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/styles/');
   eleventyConfig.addPassthroughCopy('src/');
   eleventyConfig.addPassthroughCopy('src/admin');
-  const md = require("markdown-it")({
-    html: true
-  })
-
-  eleventyConfig.addNunjucksFilter("markdownify", (markdownString) =>
-  md.render(markdownString)
-  );
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
